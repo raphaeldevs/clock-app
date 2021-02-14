8fbf871687558629881137653128d6a2457b13a4
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-02-14 01:41:00 -0300

    fixup! Create global styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -6,6 +6,8 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
 
-    font-family: 'Inter', sans-serif;
+    font: 62.5% 'Inter', sans-serif;
+
+    color: white;
   }
 `
