a0374530ff9f1352b6dc27fd0bef3d3c31466573
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-02-14 01:43:41 -0300

    create interface

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,9 +1,128 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
+
+export const IconStyles = {
+	width: "6rem",
+	height: "6rem",
+	color: "#FFFFFF"
+}
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
+	width: 100vw;
+	height: 100vh;
+
+  display: grid;
+  justify-content: space-between;
   align-items: center;
 
-  background: url('https://images.unsplash.com/photo-1534269222346-5a896154c41d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format')
-`;
+  grid-template-columns: repeat(2, 1fr); 
+  grid-template-rows: repeat(2, 1fr);
+
+  padding: 5% 7%;
+
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1534269222346-5a896154c41d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format');
+
+  background-size: cover;
+
+  section {
+  	width: 100%;
+		height: 100%;
+
+		display: flex;		
+  }
+`
+
+export const Quote = styled.section`
+	flex-direction: column;
+
+	q {
+		font-size: 2rem;
+
+		text-align: justify;
+	}
+
+	h3 {
+		margin-top: 3.5rem;
+
+		font-size: 2.3rem;
+		font-weight: 600;
+	}
+`
+
+export const Climate = styled.section`
+	justify-content: flex-end;
+	align-items: flex-start;
+
+	opacity: 0.3;	
+
+	strong {
+		margin-left: 2rem;
+
+		font-size: 6rem;
+		font-weight: 500;
+
+		line-height: 100%;
+	}
+`
+
+export const Days = styled.section`
+	justify-content: flex-end;
+	align-items: flex-end;
+
+	opacity: 0.3;
+
+	span {
+		font-size: 4rem;
+		font-weight: 500;
+
+		margin-right: 1.5rem;
+	}
+
+	strong {
+		font-size: 6.5rem;
+		font-weight: 700;
+
+		line-height: 100%;
+	}
+`
+
+export const Main = styled.main`
+	width: 150%;
+	height: 100%;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+`
+
+export const HelloMessage = styled.p`
+	display: flex;
+	align-items: center;
+
+	font-size: 4.5rem;
+	font-weight: 500;
+
+	letter-spacing: 3%;
+
+	text-transform: uppercase;
+
+	svg {
+		margin-right: 2rem;
+	}
+`
+
+export const Time = styled.time`
+	font-size: 15rem;
+	font-weight: 700;
+`
+
+export const Info = styled.p`
+	display: flex;
+	align-items: center;
+
+	font-size: 2rem;
+	font-weight: 200;
+
+	svg {
+		margin-right: 1rem;
+	}
+`
