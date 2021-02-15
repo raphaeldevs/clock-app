215c1aab3b00f7132a27e92ee45090c9a73ca50a
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-02-14 23:48:34 -0300

    add time feature

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,7 +1,31 @@
+import { useState } from 'react'
+
 import { TimeView } from './styles'
 
+import { getTime } from '../../utils'
+
 export default function Time() {
+	const [time, setTime] = useState(getTime())
+
+	const getFormattedTime = () => {
+	  const { hours, minutes } = time
+
+	  const formattedHours = hours < 10 
+			? `0${hours}`
+			: hours
+
+		const formattedMinutes = minutes < 10 
+			? `0${minutes}`
+			: minutes
+
+	  return `${formattedHours}:${formattedMinutes}`
+	}
+
+	setInterval(() => {
+		setTime(getTime())
+	}, 1000)
+
 	return (
-  	<TimeView>11:38</TimeView>
+  	<TimeView>{ getFormattedTime() }</TimeView>
 	)
 }
